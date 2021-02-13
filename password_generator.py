97b138826bed1e19de965de3a204f3a4a4e27909
pareshmeher <67202746+pareshmeher@users.noreply.github.com>
2021-02-13 17:38:10 +0530

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,13 @@
+import string
+import random
+
+def randompassword():
+    chars = string.ascii_letters + string.digits + string.punctuation
+    size = 3
+    return ''.join(random.choice(chars) for x in range(size, 27))
+
+n = 0
+while n<50:
+    print(randompassword())
+    n= n+1
+    
